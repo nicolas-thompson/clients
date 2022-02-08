285368c5522c1758ded19677bd3f01b00d5b2492
Nicolas Thompson <mail@nicolasthompson.com>
2022-02-08 21:37:28 +0000

    Makes the date human readable

--- frontend/src/components/Client.jsx
+++ frontend/src/components/Client.jsx
@@ -3,10 +3,18 @@ import css from "./Client.module.css";
 
 const Client = props => {
 
+  const day = new Date(props.client.createdDate).toLocaleString('en-GB', { day: '2-digit' });
+  const month = new Date(props.client.createdDate).toLocaleString('en-GB', { month: 'long' });
+  const year = new Date(props.client.createdDate).toLocaleString('en-GB', { year: 'numeric' });
+
   return (
 
     <div className={css.client}>
-      <div>{props.client.createdDate}</div>
+      <div>
+        <div>{day}</div>
+        <div>{month}</div>
+        <div>{year}</div>
+      </div>
       <div className={css.client__description}>
         <h2>{props.client.name}</h2>
         <div>{props.client.email}</div>
